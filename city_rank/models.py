e6efd6f31f562c2fb266a1ea758f5068e6296fb5
User <johndoe@example.com>
2020-10-31 13:11:39 +0500

    selemele

--- city_rank/models.py
+++ city_rank/models.py
@@ -1,43 +1,5 @@
+from django.contrib.auth.models import User
 from django.db import models
-
-ParamsTemplate = [
-    ('1', 'Param1'),
-    ('2', 'Param2'),
-    ('3', 'Param3'),
-    ('4', 'Param4'),
-    ('5', 'Param5'),
-    ('6', 'Param6'),
-    ('7', 'Param7'),
-    ('8', 'Param8'),
-    ('9', 'Param9'),
-    ('10', 'Param10'),
-    ('11', 'Param11'),
-    ('12', 'Param12'),
-    ('13', 'Param13'),
-    ('14', 'Param14'),
-    ('15', 'Param15'),
-    ('16', 'Param16'),
-    ('17', 'Param17'),
-    ('18', 'Param18'),
-    ('19', 'Param19'),
-    ('20', 'Param20'),
-    ('21', 'Param21'),
-    ('22', 'Param22'),
-    ('23', 'Param23'),
-    ('24', 'Param24'),
-    ('25', 'Param25'),
-    ('26', 'Param26'),
-    ('27', 'Param27'),
-    ('28', 'Param28'),
-    ('29', 'Param29'),
-    ('30', 'Param30'),
-    ('31', 'Param31'),
-    ('32', 'Param32'),
-    ('33', 'Param33'),
-    ('34', 'Param34'),
-    ('35', 'Param35'),
-    ('36', 'Param36'),
-]
 
 
 class CityParams(models.Model):
@@ -69,3 +31,25 @@ class City(models.Model):
     class Meta:
         verbose_name = 'Город'
         verbose_name_plural = 'Города'
+
+
+# class Tasks(models.Model):
+#     name = models.CharField("Наименование", max_length=100),
+#     responsible = models.ForeignKey(User,
+#                                     on_delete=models.CASCADE,
+#                                     verbose_name="Ответственный",
+#                                     related_name="res")
+#     term = models.DateField("Срок")
+#     is_completed = models.BooleanField("Завершен", default=False)
+#
+#     def __str__(self):
+#         return "{}".format(self.name)
+#
+#     class Meta:
+#         verbose_name = "Задача"
+#         verbose_name_plural = "Задачи"
+#
+#
+# class Activity(models.Model):
+#     description = models.TextField("Описание")
+#     target = models.
